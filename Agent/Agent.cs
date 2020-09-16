2fa27187e22e8825d8e368ae3801ee327fb874a8
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-09-16 12:02:21 +0200

    Add project files.

---
+++ Agent/Agent.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AgentAssignment
+{
+    public class Agent : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Notify([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        string id;
+        string codeName;
+        string speciality;
+        string assignment;
+
+        public Agent()
+        {
+        }
+
+        public Agent(string aId, string aName, string aSpeciality, string aAssignment)
+        {
+            id = aId;
+            codeName = aName;
+            speciality = aSpeciality;
+            assignment = aAssignment;
+        }
+
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                Notify();
+            }
+        }
+
+        public string CodeName
+        {
+            get
+            {
+                return codeName;
+            }
+            set
+            {
+                codeName = value;
+                Notify();
+            }
+        }
+
+        public string Speciality
+        {
+            get
+            {
+                return speciality;
+            }
+            set
+            {
+                speciality = value;
+                Notify();
+            }
+        }
+
+        public string Assignment
+        {
+            get
+            {
+                return assignment;
+            }
+            set
+            {
+                assignment = value;
+                Notify();
+            }
+        }
+    }
+}
