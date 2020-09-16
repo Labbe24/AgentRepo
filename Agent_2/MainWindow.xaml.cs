2ee66adf218be1f7ad6144e7ae9313810d55de5e
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-09-16 20:44:42 +0200

    Added Agent_2

--- Agent_2/MainWindow.xaml.cs
+++ Agent_2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,23 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Agent = AgentAssignment.Agent;
 
-namespace Agent
+namespace Agent_2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<AgentAssignment.Agent> listData = new ObservableCollection<AgentAssignment.Agent>();
         public MainWindow()
         {
             InitializeComponent();
+            listData.Add(new AgentAssignment.Agent("007","James Bond","Assassination","UpperVolta"));
+            listData.Add(new AgentAssignment.Agent("010", "Thomas Gammelby", "Blowjob", "Jens"));
+            listData.Add(new AgentAssignment.Agent("005", "Andreas Støve", "Ganks", "Nexus"));
+            this.DataContext = listData;
         }
     }
 }
